f7ceabe7d22e8a50d4463550c7dd955ebf9eaa5c
tonye0115 <tonye0115@163.com>
2016-11-03 11:01:43 +0800

    修改热度

--- src/main/scala/sparkstreaming/kafka/consumer/WebPagePopularityValueCalculator.scala
+++ src/main/scala/sparkstreaming/kafka/consumer/WebPagePopularityValueCalculator.scala
@@ -8,6 +8,8 @@ import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka.{KafkaManager, KafkaUtils}
 import org.apache.spark.streaming.{Duration, Seconds, StreamingContext}
 
+import scala.collection.GenTraversableOnce
+
 /**
   * Created by tangfei on 2016/10/28.
   */
@@ -23,6 +25,15 @@ object WebPagePopularityValueCalculator {
                              |  <topics> is a list of one or more kafka topics to consume from
                              |  <groupid> is a consume group
                              |  <processingInterval> is  execution time interval
+                             |
+                             |kafka-console-producer --broker-list ryxc164:9092 --topic user-behavior-topic
+                             |
+                             |spark-submit \\
+                             |--class sparkstreaming.kafka.consumer.WebPagePopularityValueCalculator \\
+                             |--master yarn-client \\
+                             |/home/ryxc/spark-jar/spark-demo.jar \\
+                             |ryxc163:9092,ryxc164:9092,ryxc165:9092 user-behavior-topic user-behavior-topic-message-consumer-group2 5
+
         """.stripMargin)
       System.exit(1)
     }
@@ -62,9 +73,62 @@ object WebPagePopularityValueCalculator {
 
     val msgDataRDD = kafkaStream.map(_._2)
     //for debug use only
-    //println("Coming data in this interval...")
-    msgDataRDD.print()
+    println("Coming data in this interval...")
+    //msgDataRDD.print()
 
+    // e.g page37|5|1.5119122|-1
+    val popularityData: DStream[(String, Double)] = msgDataRDD.map { msgLine => {
+        val dataArr: Array[String] = msgLine.split("\\|")
+        val pageID = dataArr(0)
+        //calculate the popularity value
+        val popValue: Double = dataArr(1).toFloat * 0.8 + dataArr(2).toFloat * 0.8 + dataArr(3).toFloat * 1
+        (pageID, popValue)
+      }
+    }
+
+    popularityData.foreachRDD(rdd=>rdd.collect.foreach(println))
+
+    val initalRDD: RDD[(String, Double)] = ssc.sparkContext.parallelize(List(("page1",0.00)))
+    val updatePopularityValue = (iterator:Iterator[(String,Seq[Double],Option[Double])]) => {
+      iterator.flatMap(t=>{
+        val newValue: Double = t._2.sum
+        val stateValue: Double = t._3.getOrElse(0)
+        Some(newValue + stateValue)
+      }.map(sumedValue=>(t._1,sumedValue)))
+    }
+
+
+    //调用 updateStateByKey 原语并传入上面定义的匿名函数更新网页热度值。
+    //val stateDStream: DStream[(String, Double)] = popularityData.updateStateByKey(updatePopularityValueFunc)
+    val stateDStream: DStream[(String, Double)] = popularityData.updateStateByKey(
+      updatePopularityValue,new HashPartitioner(ssc.sparkContext.defaultParallelism),true,initalRDD)
+
+    stateDStream.foreachRDD(rdd=>{
+      println("\n\nStateByKey:"+rdd.count())
+      //rdd.collect.foreach(println)
+    })
+
+    //set the checkpoint interval to avoid too frequently data checkpoint which may
+    //may significantly reduce operation throughput
+    stateDStream.checkpoint(Duration(8*processingInterval.toInt*1000))
+
+    //最后得到最新结果后，需要对结果进行排序，最后打印热度值最高的 10 个网页。
+    stateDStream.foreachRDD(rdd=>{
+      val sortedData: RDD[(Double, String)] = rdd.map{case (k,v) => (v,k)}.sortByKey(false)
+      val topKData : Array[(String, Double)] = sortedData.take(10).map{ case (v,k)=>(k,v)}
+      println("\n\nTopKData:");
+      topKData.foreach(println)
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+
+  def updatePopularityValueFunc=(value:Seq[Double],status:Option[Double])=>{
+    val newValue: Double = value.sum
+    val stateValue: Double = status.getOrElse(0)
+    Some(newValue+stateValue)
   }
 
 
