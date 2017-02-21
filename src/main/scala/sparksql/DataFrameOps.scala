98f1826b210a09e650490e070da4ffdff649c92a
tonye0115 <tonye0115>
2017-02-21 09:40:49 +0800

    sparksql功能提交

---
+++ src/main/scala/sparksql/DataFrameOps.scala
@@ -0,0 +1,34 @@
+package sparksql
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by tonye0115 on 2017/2/21.
+  *
+  */
+object DataFrameOps {
+  """
+    |spark-submit \\
+    |--class sparksql.DataFrameOps \\
+    |--master yarn-client \\
+    |/root/spark-demo.jar
+  """.stripMargin
+  def main(args: Array[String]) {
+    val conf = new SparkConf()
+    conf.setAppName("Spark hello!")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    val df = sqlContext.read.json("hdfs://nameservice1/library/examples/src/main/resources/people.json")
+    df.show()
+    df.printSchema()
+
+    df.select("name").show()
+    df.select(df("name"), df("age") + 10).show()
+    df.filter(df("age") > 10).show()
+    df.groupBy("age").count().show()
+
+
+  }
+
+}
