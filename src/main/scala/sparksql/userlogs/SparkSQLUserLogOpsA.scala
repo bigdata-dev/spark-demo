b0647904b5a077eaa9d3b20b1db276fc332c13d8
tonye0115 <tonye0115>
2017-03-01 21:47:09 +0800

    sparksql功能提交

--- src/main/scala/sparksql/userlogs/SparkSQLUserLogOpsA.scala
+++ src/main/scala/sparksql/userlogs/SparkSQLUserLogOpsA.scala
@@ -15,16 +15,51 @@ object SparkSQLUserLogOpsA {
     * 以下代码是在 spark-shell命令下执行
     * 因为CDH没有spark-sql命令，只能借助spark-shell在hiveContext中执行
     */
-  val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
-  hiveContext.sql("DROP TABLE IF EXISTS  userLogs")
-  hiveContext.sql("CREATE TABLE IF NOT EXISTS  " +
+  val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
+  sqlContext.sql("DROP TABLE IF EXISTS  userLogs")
+  sqlContext.sql("CREATE TABLE IF NOT EXISTS  " +
     "userLogs(date STRING, timestamp BIGINT, userID INT,pageID INT,channel STRING,action  STRING) " +
     "ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\t' LINES TERMINATED BY '\\n' STORED AS TEXTFILE")
 
-  hiveContext.sql("DESC formatted  userLogs").show()
+  sqlContext.sql("DESC formatted  userLogs").show()
 
-  hiveContext.sql("LOAD DATA LOCAL INPATH  '/var/lib/hadoop-hdfs/tmp/userLog.log'" +
+  sqlContext.sql("LOAD DATA LOCAL INPATH  '/var/lib/hadoop-hdfs/tmp/userLog.log'" +
     " INTO TABLE userLogs")
+
+  //统计pv  (每个页面有多少次访问)
+  sqlContext.sql("select date,pageID, pv from ( " +
+    " select ul.date, ul.pageId, count(*) as pv " +
+    " from userLogs ul " +
+    " where ul.action='View' and ul.date='2017-02-26' group by ul.date,ul.pageID ) subquery" +
+    " order by pv desc ").show()
+
+
+
+
+  //统计uv  (每个页面有多少个用户访问)
+  sqlContext.sql("select date,pageID, pv from ( " +
+    " select ul.date, ul.pageId, count(distinct(userID)) as pv " +
+    " from userLogs ul " +
+    " where ul.action='View' and ul.date='2017-02-26' group by ul.date,ul.pageID ) subquery" +
+    " order by pv desc ").show()
+
+
+
+  //统计总pv collect 返回的是org.apache.spark.sql.ROW
+  val pvCount = sqlContext.sql("select count(*) from userLogs ul" +
+    " where ul.action='View' and ul.date='2017-02-26'").collect()
+
+  val totalTargetPV = pvCount(0).get(0)
+
+  //用户跳出率 (今天访问页面只有一次)
+  val targetResult =  sqlContext.sql("select count(*) from (select count(*) totalNumber from userLogs where action = 'View' and date='2017-02-26'" +
+    " group by userID having totalNumber=1) targetTable ").collect()
+  val pv1 = targetResult(0).get(0)
+  val percent = pv1.toString.toDouble/totalTargetPV.toString.toDouble
+
+
+
+
 
 
 }
